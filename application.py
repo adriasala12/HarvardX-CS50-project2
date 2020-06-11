0c2b7568f9423dad2bcdec6a323b397602453f34
Adrià Sala <adriasala124@gmail.com>
2020-06-12 00:24:37 +0200

    Import Session

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+from flask_session import Session
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
