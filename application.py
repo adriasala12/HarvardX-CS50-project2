348fd2b0761130708c4113a4be64875c0c543362
Adrià Sala <adriasala124@gmail.com>
2020-06-12 01:19:00 +0200

    Session save user's name working

--- application.py
+++ application.py
@@ -1,22 +1,27 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask_socketio import SocketIO, emit
-from flask_session import Session
+#from flask_session import Session
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-app.config["SESSION_PERMANENT"] = false
+app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+#Session(app)
+app.secret_key = 'asr'
+#session['name'] = None
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if 'name' in session:
+        return render_template("home.html", name=session['name'])
+    else:
+        return render_template("index.html")
 
 @app.route("/home", methods=["POST"])
 def home():
-    name = request.form.get("name")
-    return render_template("home.html", name=name)
+    session['name'] = request.form.get("name")
+    return render_template("home.html", name=session['name'])
